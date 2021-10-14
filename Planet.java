5c26910e546cf6b63e3988b39e5f1407d4d8560a
Evans <evanssc@egb080000020.lancs.local>
2021-10-14 17:11:18 +0100

    Tried to add new moon child class and failed, Added new javadoc commenting on planet

--- Planet.java
+++ Planet.java
@@ -1,5 +1,6 @@
 public class Planet
 {
+    //Establishing variables
     int distance_from_sun;
     int angle;
     int diameter;
@@ -7,8 +8,18 @@ public class Planet
     SolarSystem sys;
     int orbit_speed;
 
+    /**
+     * Creates a planet object to be presented on the SolarSystem object which has been passed through
+     * @param d The distance from the sun of the planet which will be made
+     * @param a The angle from zero (polar) which the planet is at
+     * @param dia The diameter of the planet
+     * @param c The Colour of the planet
+     * @param s The SolarSystem object from Main
+     * @param o The orbital speed of the object
+     */
     public Planet(int d, int a, int dia, String c, SolarSystem s, int o)
     {
+        //Setting variables
         distance_from_sun = d;
         angle = a;
         diameter = dia;
@@ -17,19 +28,29 @@ public class Planet
         orbit_speed = o;
     }
 
-    public void Orbit()
+    /**
+     * Changes the angle of the object, therefore moving it on the solarSystem object
+     * @param speed The rate at which the angle changes
+     */
+    public void Orbit(int speed)
     {
-        //rotating clockwise
-        angle-= orbit_speed;
+        angle-= speed;
     }
 
-    public int getAngle()
-    {
-        return(angle);
-    }
-
+    /**
+     * Draws the object onto the board
+     */
     public void draw()
     {
         sys.drawSolarObject(distance_from_sun,angle,diameter,colour);
+    }
+
+    /**
+     * Finds the speed at which the angle is changing
+     * @return Change in pixels every loop
+     */
+    public int getSpeed()
+    {
+        return orbit_speed;
     }
 }
--- main.java
+++ main.java
@@ -3,21 +3,26 @@ public class main
     public static void main(String args[])
     {
         SolarSystem f = new SolarSystem(300,300);
-        Planet earth = new Planet(100,0,10,"BLUE",f,2);
-        Planet mars = new Planet(65,0,5,"RED",f,1);
+        Planet[] planets = new Planet[2];
+        planets[0] = new Planet(100,0,10,"BLUE",f,2); //earth
+        planets[1] = new Planet(65,0,5,"RED",f,1); //mars
+        planets[2] = new Moon(5, 0, 5, "GREY", f, 1);
+
+        planets[2].setParent(planets[0]);
 
         //drawing the sun
-        f.drawSolarObject(0,0,20,"YELLOW");
+        f.drawSolarObject(0,0,20,"YELLOW"); //the sun (centerpoint)
 
         while (true)
         {
             f.drawSolarObject(0,0,20,"YELLOW");
-            earth.draw();
-            mars.draw();
-            f.finishedDrawing();
-            earth.Orbit();
-            mars.Orbit();
+            for(Planet p:planets)
+            {
+                p.draw();
+                p.Orbit(p.getSpeed());
+            }
 
+            f.finishedDrawing();
         }
     }
 }
